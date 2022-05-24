1e2167033be1bed5488595e3c3d0313caf458036
AbdElrahmanGbr <abdelrahman.gbr.os@gmail.com>
2022-05-24 14:31:08 +0200

    Lab1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderContainerComponent } from './header-container/header-container.component';
+import { HeaderComponent } from './header/header.component';
+import { NavComponent } from './nav/nav.component';
+import { AsideComponent } from './aside/aside.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderContainerComponent,
+    HeaderComponent,
+    NavComponent,
+    AsideComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
